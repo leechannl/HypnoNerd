bfe2c9eda5984505ffae1af096b42f5497e8e637
leechannl <leechannl@gmail.com>
2014-06-05 17:59:42 +0800

    Initial Commit

---
+++ HypnoNerd/MMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMAppDelegate.h
+//  HypnoNerd
+//
+//  Created by mm on 6/5/14.
+//  Copyright (c) 2014 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
